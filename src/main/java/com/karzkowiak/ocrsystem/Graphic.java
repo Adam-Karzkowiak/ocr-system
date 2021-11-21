9b263deecfae9d98f06be3c66a5fd1128e82cdaf
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-11-21 22:00:43 +0100

    id for Graphic entity

--- src/main/java/com/karzkowiak/ocrsystem/Graphic.java
+++ src/main/java/com/karzkowiak/ocrsystem/Graphic.java
@@ -2,9 +2,20 @@ package com.karzkowiak.ocrsystem;
 
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
+@Entity
 public class Graphic {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String url;
     private String content;
+
+
 }
